596c56bd2f4f045865c840398059800a90d7bdf5
RWejlgaard <wejlgaard@protonmail.com>
2017-08-22 13:01:08 +0200

    remade listener.py

--- pillar/mariadb.sls
+++ pillar/mariadb.sls
@@ -2,7 +2,7 @@ mariadb:
   version: 'latest'
   repo_version: '10.1'
   server:
-    root_password: password
+    root_password: N2F6jfBm
     config:
       mysqld:
         bind-address: 0.0.0.0
--- salt/listener.py
+++ salt/listener.py
@@ -1,12 +1,49 @@
-from time import sleep
+import time
 import salt
 import salt.client
+import salt.config
+import salt.utils.event
+import mysql.connector
 
-def listen():
-    pass
+IGNORE_LIST = ['salt/event/exit']
 
+
+def start_listener():
+    opts = salt.config.client_config('/etc/salt/master')
+    sevent = salt.utils.event.get_event(
+        'master',
+        sock_dir=opts['sock_dir'],
+        transport=opts['transport'],
+        opts=opts
+    )
+
+    print "Listener Started!"
+
+    while True:
+        try:
+            ret = sevent.get_event(full=True)
+            if ret is None:
+                continue
+            else:
+                handle_event(ret)
+            time.sleep(2)
+        except KeyboardInterrupt:
+            quit()
+
+
+def handle_event(e):
+    try:
+        if e['tag'] not in IGNORE_LIST:
+            print "Got event\t{}".format(e['tag'])
+            con = mysql.connector.connect(user='events', password='N2F6jfBm', host='ymir.wejlgaard.com', database='salt')
+            cursor = con.cursor()
+            query = """INSERT INTO events (tag, data) VALUES ("{}", "{}");""".format(str(e['tag']), str(e['data']))
+            cursor.execute(query)
+            con.commit()
+            print "Posted event\t{}".format(e['tag'])
+            con.close()
+    except Exception as e:
+        print e
 
 if __name__ == '__main__':
-    while True:
-        listen()
-        sleep(30)
+    start_listener()
